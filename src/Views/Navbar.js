576b9155059fb6e288d3149f25eae29955002cd3
Mohamed Osama <mohamed.osama@g2k-group.com>
2021-11-27 03:10:23 +0200

    fix logo

--- src/Views/Navbar.js
+++ src/Views/Navbar.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react'
-
+import Logo from '../Images/netflix.png'
 export class navbar extends Component {
     render() {
         return (
             <div className="flex  h-[70px] md:px-4 items-center">
-                <img className="mx-4 h-3/4" src="/netflix2.png" alt="Logo"></img>
+                <img className="mx-4 h-3/4" src={Logo} alt="Logo"></img>
                 <div className="navBtn">Home</div>
                 <div className="navBtn">TV Shows</div>
                 <div className="navBtn">Movies</div>
