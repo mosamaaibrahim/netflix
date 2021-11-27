80290a49820c96cdcfb4c5b1a0e3bcca5f442b4a
Mohamed Osama <mohamed.osama@g2k-group.com>
2021-11-27 16:55:46 +0200

    adding link to logo

--- src/Views/Navbar.js
+++ src/Views/Navbar.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react'
 import Logo from '../Images/netflix.png'
+import { Link } from 'react-router-dom'
 export class navbar extends Component {
     render() {
         return (
             <div className="flex  h-[70px] md:px-4 items-center">
-                <img className="mx-4 h-3/4" src={Logo} alt="Logo"></img>
+                <Link to="/" className="mx-4 h-3/4">
+                    <img src={Logo} alt="Logo" className="h-full"></img>
+                </Link>
                 <div className="navBtn">Home</div>
                 <div className="navBtn">TV Shows</div>
                 <div className="navBtn">Movies</div>
