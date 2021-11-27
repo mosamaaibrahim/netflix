40bc4b154240957c27e1bf78673119e3f3ac23af
Mohamed Osama <mohamed.osama@g2k-group.com>
2021-11-27 03:01:23 +0200

    fix homepage

--- src/Views/Navbar.js
+++ src/Views/Navbar.js
@@ -4,7 +4,7 @@ export class navbar extends Component {
     render() {
         return (
             <div className="flex  h-[70px] md:px-4 items-center">
-                <img className="mx-4 h-3/4" src="/netflix.png" alt="Logo"></img>
+                <img className="mx-4 h-3/4" src="/netflix2.png" alt="Logo"></img>
                 <div className="navBtn">Home</div>
                 <div className="navBtn">TV Shows</div>
                 <div className="navBtn">Movies</div>
