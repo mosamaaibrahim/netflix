196bb79f29661bd0ff9b6b22730e67dd7a568814
Mohamed Osama <mohamed.osama@g2k-group.com>
2021-11-27 15:50:26 +0200

    adding shadow to mainShow slider

--- src/Components/MainShow.js
+++ src/Components/MainShow.js
@@ -14,45 +14,14 @@ export class MainShow extends Component {
     state = {
         video: null
     }
-    componentDidUpdate = (prevProps) => {
-        // if (JSON.stringify(prevProps.show) !== JSON.stringify(this.props.show)) {
-        //     this.getVideo();
-        // }
-    }
-    // getVideo = () => {
-    //     _getVideos(this.props.show.id)
-    //         .then(res => {
-    //             let video = res.data.results.find(vid => vid.site === "YouTube")
-    //             if (video) {
-    //                 this.setState({ video })
-    //             }
-    //         })
-    // }
+
     render() {
 
         return (
             <div className="md:h-[60vh] w-full relative overflow-hidden rounded"
             >
-
-                {/* {
-                    this.state.video ?
-                        <iframe className="w-full h-full"
-                            title="Main Trailer"
-                            src={`https://www.youtube.com/embed/${this.state.video?.key}?loop=1&autoplay=1&mute=1&controls=0`}>
-                        </iframe> :
-                        <>
-                            <img
-                                className=""
-                                src={`https://image.tmdb.org/t/p/original/${show?.backdrop_path}`} alt="" />
-                            <div className="absolute left-[4%] top-[20%] ">
-                                <h1 className="text-3xl text-white font-extrabold">{show?.original_title}</h1>
-                                <p className="text-white w-[50%]">{show?.overview}</p>
-                            </div>
-                        </>
-                } */}
                 <Swiper
                     modules={[Navigation, Pagination, Autoplay]}
-                    // spaceBetween={10}
                     slidesPerView={1}
                     freeMode
                     autoplay={{ delay: 5000 }}
@@ -65,8 +34,12 @@ export class MainShow extends Component {
                                 <img
                                     className="w-auto"
                                     src={`https://image.tmdb.org/t/p/original/${show?.backdrop_path}`} alt="" />
-                                <div className="absolute left-[1%] md:left-[4%] top-[65%] md:top-[20%] ">
+                                {/* <div className="absolute left-[1%] md:left-[4%] top-[65%] md:top-[20%] ">
                                     <h1 className="text-lg md:text-3xl text-white font-extrabold">{show?.original_title}</h1>
+                                    <p className="hidden md:flex text-sm text-white w-[50%]">{show?.overview}</p>
+                                </div> */}
+                                <div className="absolute top-0 w-full h-full flex flex-col  justify-end pb-[5%] pl-[5%]  md:h-[60vh] bg-gradient-to-t from-prblack">
+                                    <h1 className="text-lg md:text-3xl text-white font-extrabold md:mb-4">{show?.original_title}</h1>
                                     <p className="hidden md:flex text-sm text-white w-[50%]">{show?.overview}</p>
                                 </div>
                             </></SwiperSlide>
