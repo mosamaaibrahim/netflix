698ebf2d8e8c996bf72254f1be1f2b57ce282683
Mohamed Osama <mohamed.osama@g2k-group.com>
2021-11-28 02:55:00 +0200

    adding single movie credits / icons

--- src/Views/SingleMovie.js
+++ src/Views/SingleMovie.js
@@ -1,20 +1,24 @@
 import React, { Component } from 'react'
 
 import { getPoster } from '../Utils/constants'
-import { _getMoviesDetails, _getVideos } from '../API/moviesApi'
+import { _getMoviesDetails, _getVideos, _getMoviesCredits } from '../API/moviesApi'
+import StarIcon from '../Images/star.png'
+import VoteIcon from '../Images/vote.png'
+import ArrowIcon from '../Images/arrow.png'
 export class SingleMovie extends Component {
     state = {
         movie: null,
-        video: null
+        video: null,
+        cast: []
     }
     componentDidMount = () => {
         let movieId = this.props.match?.params?.id
         _getMoviesDetails(movieId).then((res) => {
-            this.setState({ movie: res.data }, () => this.getVideo())
+            this.setState({ movie: res.data }, () => this.movieDetails())
         })
     }
 
-    getVideo = () => {
+    movieDetails = () => {
         _getVideos(this.state.movie?.id)
             .then(res => {
                 let video = res.data.results.find(vid => vid.site === "YouTube")
@@ -22,15 +26,29 @@ export class SingleMovie extends Component {
                     this.setState({ video })
                 }
             })
+
+        _getMoviesCredits(this.state.movie?.id)
+            .then(res => {
+                this.setState({ cast: res.data.cast.slice(0, 9) })
+            })
+
     }
     render() {
         return (
-            <div className="grid grid-cols-5 gap-5 p-8 ">
+            <div className="grid grid-cols-1 md:grid-cols-5 md:gap-5 p-8 ">
                 <h1 className="text-3xl font-extrabold text-white col-span-4 items-center">{this.state.movie?.original_title}</h1>
-                <div className="flex flex-row justify-between items-center">
-                    <div className="text-white">{this.state.movie?.vote_average}</div>
-                    <div className="text-white">{this.state.movie?.vote_count}</div>
-                    <div className="text-white">{this.state.movie?.popularity}</div>
+                <div className="flex flex-row font-bold justify-between items-center col-span-1">
+                    <div className="text-white flex flex-row items-center justify-around">{this.state.movie?.vote_average}
+                        <img src={StarIcon} alt="Star" className="h-5 mx-2" />
+                    </div>
+                    <div className="text-white font-bold flex flex-row items-center justify-around">{this.state.movie?.vote_count}
+                        <img src={VoteIcon} alt="Star" className="h-5 mx-2" />
+                    </div>
+                    <div className="text-white font-bold flex flex-row items-center justify-around ">{Math.floor(this.state.movie?.popularity)}
+                        <div className="bg-red-700 rounded-full p-1 h-5 mx-2">
+                            <img src={ArrowIcon} alt="Star" className="h-full" />
+                        </div>
+                    </div>
                 </div>
                 <img src={getPoster(this.state.movie?.poster_path)} alt="Poster"
                     className="col-span-1 max-h-[50vh] rounded"
@@ -40,8 +58,22 @@ export class SingleMovie extends Component {
                     src={`https://www.youtube.com/embed/${this.state.video?.key}?controls=0&autoplay=1&mute=1&loop=1&playlist=${this.state.video?.key}`}>
                 </iframe>
 
-                <div className="text-white text-lg col-span-3">
+                <div className="text-white text-lg col-span-2">
                     {this.state.movie?.overview}
+                </div>
+                <div className="col-span-3 grid md:grid-cols-3 md:grid-rows-3 items-center  gap-4">
+                    {
+                        this.state.cast.map(char =>
+                            <div className="flex flex-row h-[100px] justify-start items-center rounded-3xl overflow-hidden shadow-lg hover:bg-gray-800 cursor-pointer">
+                                <img
+                                    className="h-full"
+                                    src={getPoster(char.profile_path)} alt="Cast" />
+                                <div className="ml-5">
+                                    <p className="text-base text-white">{char.original_name}</p>
+                                    <p className="text-sm text-gray-600">{`As ${char.character}`}</p>
+                                </div>
+                            </div>)
+                    }
                 </div>
             </div>
         )
