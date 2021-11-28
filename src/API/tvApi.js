86ec82d5fda2af8e6ca5cad447e416d6f92efd79
Mohamed Osama <mohamed.osama@g2k-group.com>
2021-11-28 19:28:28 +0200

    adding tv api

--- src/API/tvApi.js
+++ src/API/tvApi.js
@@ -42,3 +42,66 @@ export function _getTvToprated() {
             });
     });
 }
+
+export function _getTvDetails(id) {
+    return new Promise((res, rej) => {
+        axios({
+            method: "get",
+            url: `/tv/${id}?api_key=${API_KEY}`,
+            data: {},
+            headers: {
+                "Cache-Control": "no-cache",
+                "Content-Type": "application/json",
+            },
+        })
+            .then((responseText) => {
+                res(responseText);
+            })
+            .catch((error) => {
+                console.error(`_getTvDetails : ${error}`);
+                rej(error);
+            });
+    });
+}
+
+export function _getVideos(id) {
+    return new Promise((res, rej) => {
+        axios({
+            method: "get",
+            url: `/tv/${id}/videos?api_key=${API_KEY}`,
+            data: {},
+            headers: {
+                "Cache-Control": "no-cache",
+                "Content-Type": "application/json",
+            },
+        })
+            .then((responseText) => {
+                res(responseText);
+            })
+            .catch((error) => {
+                console.error(`_getVideos : ${error}`);
+                rej(error);
+            });
+    });
+}
+
+export function _getTvCast(id) {
+    return new Promise((res, rej) => {
+        axios({
+            method: "get",
+            url: `/tv/${id}/credits?api_key=${API_KEY}`,
+            data: {},
+            headers: {
+                "Cache-Control": "no-cache",
+                "Content-Type": "application/json",
+            },
+        })
+            .then((responseText) => {
+                res(responseText);
+            })
+            .catch((error) => {
+                console.error(`_getTvCast : ${error}`);
+                rej(error);
+            });
+    });
+}
