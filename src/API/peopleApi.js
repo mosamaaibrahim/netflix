a1d568adebc051d516599bacf32bd06093a764b6
Mohamed Osama <mohamed.osama@g2k-group.com>
2021-11-29 02:41:21 +0200

    adding people api

--- src/API/peopleApi.js
+++ src/API/peopleApi.js
@@ -22,3 +22,45 @@ export function _getPopularPeople() {
             });
     });
 }
+
+export function _getPeopleDetails(id) {
+    return new Promise((res, rej) => {
+        axios({
+            method: "get",
+            url: `/person/${id}?api_key=${API_KEY}`,
+            data: {},
+            headers: {
+                "Cache-Control": "no-cache",
+                "Content-Type": "application/json",
+            },
+        })
+            .then((responseText) => {
+                res(responseText);
+            })
+            .catch((error) => {
+                console.error(`_getPeopleDetails : ${error}`);
+                rej(error);
+            });
+    });
+}
+
+export function _getPersonMovies(id) {
+    return new Promise((res, rej) => {
+        axios({
+            method: "get",
+            url: `/person/${id}/movie_credits?api_key=${API_KEY}`,
+            data: {},
+            headers: {
+                "Cache-Control": "no-cache",
+                "Content-Type": "application/json",
+            },
+        })
+            .then((responseText) => {
+                res(responseText);
+            })
+            .catch((error) => {
+                console.error(`_getPersonMovies : ${error}`);
+                rej(error);
+            });
+    });
+}
